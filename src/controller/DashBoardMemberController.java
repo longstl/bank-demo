dd852622753d99ef68c81eb2d4a214bd9cbbb903
hackluv_1 <jonnykhanh1102@gmail.com>
2018-04-04 19:29:26 +0700

    add alert sign out button

--- src/controller/DashBoardMemberController.java
+++ src/controller/DashBoardMemberController.java
@@ -4,10 +4,15 @@ import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import javax.swing.*;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class DashBoardMemberController implements Initializable {
@@ -44,10 +49,21 @@ public class DashBoardMemberController implements Initializable {
     }
 
     public void actSignOut(ActionEvent actionEvent) {
-        JOptionPane.showMessageDialog(null, "Goodbye!!! See you again!!!");
-        formController.signInForm();
-        Stage stage = (Stage) btnSignOut.getScene().getWindow();
-        stage.hide();
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Sign Out Notice");
+        alert.setHeaderText("Do you want to Sign out?");
+        alert.setContentText("Please Choice Yes or No");
+        ButtonType buttonTypeYes = new ButtonType("Yes", ButtonBar.ButtonData.YES);
+        ButtonType buttonTypeNo = new ButtonType("No", ButtonBar.ButtonData.NO);
+        alert.getButtonTypes().setAll(buttonTypeYes,buttonTypeNo);
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == buttonTypeYes){
+            JOptionPane.showMessageDialog(null, "Goodbye!!! See you again!!!");
+            formController.signInForm();
+            Stage stage = (Stage) btnSignOut.getScene().getWindow();
+            stage.hide();
+        }else{
+        }
     }
 
 
