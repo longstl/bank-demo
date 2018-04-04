76ab7cca0a7c2faffaf3f3b9c1dea98914af7357
hackluv_1 <jonnykhanh1102@gmail.com>
2018-04-04 14:04:15 +0700

    Add model/connectionHelper
    Add model/connectionHelper

---
+++ src/model/DbConnection.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+
+    private Connection connection = null;
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/assignment_data", "root", "");
+            System.out.println("Connection Success!!!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Can not connect Database!!!");
+            connection = null;
+        }
+        return connection;
+    }
+}
